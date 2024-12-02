0a0e7eebaa2bce2c9fa06539f5481dc7c6b32fdb
DvirYBria <74497787+DvirYBria@users.noreply.github.com>
2024-12-02 15:33:52 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,11 +3,9 @@ from .bria_api_node import EraserNode, GenerativeFillNode
 # Map the node class to a name used internally by ComfyUI
 NODE_CLASS_MAPPINGS = {
     "BriaEraser": EraserNode,  # Return the class, not an instance
-    "BriaGenerativeFill": GenerativeFillNode,
 }
 
 # Map the node display name to the one shown in the ComfyUI node interface
 NODE_DISPLAY_NAME_MAPPINGS = {
     "BriaEraser": "Bria Eraser",
-    "BriaGenerativeFill": "Bria Generative Fill",
 }
